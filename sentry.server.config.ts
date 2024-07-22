ac7936647ac68117c92de16f801d38f82d98c6f0
Ashikam263 <ashikam263@gmail.com>
2024-07-23 00:40:22 +0530

    sentry filees default

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://c6f03f3634fe0c8797415ce3105cf88e@o4507646748393472.ingest.us.sentry.io/4507646750162944",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -15,4 +15,5 @@ Sentry.init({
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
+  
 });
